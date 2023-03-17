74123bcc2c933fcb79c961e08823f99f22969f2a
timofruehwirth <xml.codemonkey@gmail.com>
2023-03-17 15:17:41 +0100

    add digital-bio-2023.js, add font-size and image-size toggle, add HTML lang attribute to text-carrying elements

---
+++ digital-bio-2023.js
@@ -0,0 +1,6 @@
+function increaseSize() {
+    var el1 = document.body;
+    el1.classList.toggle("large-font-size")
+    var el2 = document.getElementById("network")
+    el2.classList.toggle("large-image")
+}
