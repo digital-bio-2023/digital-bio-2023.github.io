65f4a3e62dc2ab026c0bad940b5619e4438e6ad2
timofruehwirth <xml.codemonkey@gmail.com>
2023-03-30 12:04:44 +0200

    add imprint.html, cookie consent html css js

--- digital-bio-2023.js
+++ digital-bio-2023.js
@@ -1,6 +1,12 @@
+/*
+
+font size toggle
+
 function increaseSize() {
     var el1 = document.body;
     el1.classList.toggle("large-font-size")
     var el2 = document.getElementById("network")
     el2.classList.toggle("large-image")
 }
+
+*/
