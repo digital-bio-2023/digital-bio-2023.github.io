052d06ec3a6739a08cac78258b1f83b5a4e840dd
timofruehwirth <xml.codemonkey@gmail.com>
2023-04-14 11:49:28 +0200

    set viewport in htmls, add responsive topnav to index.html

--- digital-bio-2023.js
+++ digital-bio-2023.js
@@ -10,3 +10,15 @@ function increaseSize() {
 }
 
 */
+
+/*responsive navbar*/
+
+/*toggle adding/removing "responsive" class to .topnav when fa fa bar icon is clicked*/
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+} 
